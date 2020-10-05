db2a196ddc92b5918fe3491c308dd197921b8b7e
mmcloos <mmcloos@uno.edu>
2020-10-04 22:52:02 -0500

    code from Module 1

---
+++ Module1.py
@@ -0,0 +1,38 @@
+# Module 1
+# Maggie Cloos
+
+
+# int
+num1 = 1
+
+print(num1)
+
+# float
+num2 = 1.1
+
+print(num2)
+
+# boolean
+
+status = True
+
+print(status)
+
+# string
+mystr = "hello class"
+
+print(mystr)
+
+# tuple
+
+mytup = (1, 2, 3, 4)
+
+print(mytup)
+
+# dictionary
+
+mydict = {"key1": 1, "key2": 2, "key3": 3}
+
+print(mydict)
+
+
